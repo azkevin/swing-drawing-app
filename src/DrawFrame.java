ca73208cf3052013471cefb7b6ff7dc87d7c41df
azkevin <karindaeng@gmail.com>
2015-12-02 04:40:59 -0500

    - Created a seperate class for ToolBar

--- src/DrawFrame.java
+++ src/DrawFrame.java
@@ -56,31 +56,14 @@ class DrawFrame extends JFrame implements MouseMotionListener, MouseListener, Ac
 	private JPanel contentPane;	
 	private PaintPanel inkPanel;
 	private JMenuBar menuBar;
+	private JToolBar toolBar;
+	
 	final int MAX_SAMPLES = (30000); //Sufficient number of samples for points to draw.
 
-	private JButton select;
-	private JButton open;
-	private JButton pencil;
-	private JButton line;
-	private JButton rectangle;
-	private JButton circle;
-	private JButton oval;
-	private JButton rotateLeft;
-	private JButton rotateRight;
-	private JButton flipHorizontal;
-	private JButton flipVertical;
-	private JButton text;
-	private JButton erase;
-	private JButton fill;
-	private JButton undo;
-	private JButton redo;
-	private JButton clear;
 	
 	private JComboBox comboBox;
 	//private JButton thicker;
 	//private JButton thinner;
-
-	private JToolBar toolBar;
 	
 	private CoordinateBar coordinateBar;
 	
@@ -155,10 +138,8 @@ class DrawFrame extends JFrame implements MouseMotionListener, MouseListener, Ac
 		
 
 		// create a tool bar		
-		toolBar = new JToolBar(JToolBar.VERTICAL);
-		toolBar.setFloatable(false);		
-		this.initializeToolBar(toolBar);
-		
+		toolBar = (new ToolBar(this, inkPanel)).getToolBar();
+
 		// create coordinate bar at the bottom
 		coordinateBar = new CoordinateBar();
 		
@@ -227,78 +208,13 @@ class DrawFrame extends JFrame implements MouseMotionListener, MouseListener, Ac
 		
 		// add listeners to buttons
 		this.addWindowListener(new WindowCloser());
-		clear.addActionListener(this);
-		rectangle.addActionListener(this);
-		line.addActionListener(this);
-		circle.addActionListener(this);
-		erase.addActionListener(this);
-		pencil.addActionListener(this);
-		comboBox.addActionListener(this);
+
 		
 		// set components into the contentPane
 		this.setContentPane(contentPane);
 		this.setPreferredSize(new Dimension(CONTENT_PANE_WIDTH,CONTENT_PANE_HEIGHT));
 	}
 
-	private void initializeToolBar(JToolBar toolBar) {
-		toolBar.setBackground(Color.gray);
-		// ----------------
-		// create buttons for the tool bar
-		// ----------------
-		
-		select = new JButton("Select");
-		//open = new JButton("Open");
-		pencil = new JButton("Pencil");
-		line = new JButton("Line",new ImageIcon(this.getClass().getResource("/icons/Line-24.png")));
-		rectangle = new JButton("Rectangle",new ImageIcon(this.getClass().getResource("/icons/Rectangle-24.png")));
-		circle = new JButton("Circle",new ImageIcon(this.getClass().getResource("/icons/Circled.png")));
-		//oval = new JButton("Oval");
-		rotateLeft = new JButton("Rotate left",new ImageIcon(this.getClass().getResource("/icons/Rotate Left-24.png")));
-		rotateRight  = new JButton("Rotate right",new ImageIcon(this.getClass().getResource("/icons/Rotate Right-24.png")));
-		flipHorizontal = new JButton("Flip horizontal",new ImageIcon(this.getClass().getResource("/icons/Flip Horizontal-24.png")));
-		flipVertical  = new JButton("Flip vertical",new ImageIcon(this.getClass().getResource("/icons/Flip Vertical-24.png")));
-		text = new JButton("Text");
-		erase = new JButton("Erase",new ImageIcon(this.getClass().getResource("/icons/Eraser-24.png")));
-		fill = new JButton("Fill",new ImageIcon(this.getClass().getResource("/icons/Fill Color-24.png")));
-		undo = new JButton("Undo",new ImageIcon(this.getClass().getResource("/icons/Undo-24.png")));
-		redo = new JButton("Redo",new ImageIcon(this.getClass().getResource("/icons/Redo-24.png")));
-		clear = new JButton("Clear");
-		
-		select.setBackground(background);
-		//open.setBackground(background);
-		pencil.setBackground(background);
-		
-		String[] items = {"2","3","4","5","6","7","8","9","10"};
-		comboBox = new JComboBox(items);
-		
-		// ----------------
-		// add buttons to the tool bar
-		// ----------------
-		
-		toolBar.add(select);
-		//toolBar.add(open);
-		toolBar.addSeparator();
-		toolBar.add(pencil);
-		toolBar.add(line);
-		toolBar.add(rectangle);
-		toolBar.add(circle);
-		//toolBar.add(oval);
-		toolBar.addSeparator();
-		toolBar.add(rotateLeft);
-		toolBar.add(rotateRight);
-		toolBar.add(flipHorizontal);
-		toolBar.add(flipVertical);
-		toolBar.addSeparator();
-		toolBar.add(text);
-		toolBar.add(erase);
-		toolBar.add(fill);
-		toolBar.addSeparator();
-		toolBar.add(undo);
-		toolBar.add(redo);
-		toolBar.addSeparator();
-		toolBar.add(clear);
-		toolBar.add(comboBox);
-	}
 	private void initializeColorChooser(JPanel panel) {
 		turquoise = new JButton("");
 		turquoise.setPreferredSize(new Dimension(50,50));
@@ -390,86 +306,9 @@ class DrawFrame extends JFrame implements MouseMotionListener, MouseListener, Ac
 	}
 	
 	
-	// implement ActionListener method (initially run)
-	@Override
-	public void actionPerformed(ActionEvent ae)
-	{
-		Object source = ae.getSource();
-
-		if (source == clear){
-			inkPanel.clear();
-		}
-		else if (source == pencil){
-			inkPanel.setTool(0);
-		}
-		else if (source == line) {
-			inkPanel.setTool(1);
-		}
-		else if (source == rectangle) {
-			inkPanel.setTool(2);
-		}
-		else if (source == circle) {
-			inkPanel.setTool(3);
-		}
-		else if (source == select){
-			inkPanel.setTool(4);
-		}
-		else if (source == text){
-			inkPanel.setTool(5);
-		}
-		else if (source == erase){
-			inkPanel.eraser();
-		}
-//		else if (source == thicker){
-//		
-//			inkPanel.thicker();
-//		}
-//		else if (source == thinner){
-//	
-//			inkPanel.thinner();
-//		}
-		else if (source == comboBox){
-			System.out.println(1);
-			JComboBox combo = (JComboBox)ae.getSource();
-			String current = (String) combo.getSelectedItem();
-			inkPanel.setThickness(Float.valueOf(current) );
-		}
-		else{
-			JButton b = (JButton) source;
-			inkPanel.setColor(b.getBackground());
-		}
-	}
 	
 	public void stateChanged(ChangeEvent e){
 		//inkPanel.setColor(cc.getColor());
-	}
-	private void openFile(File f) {
-
-		// ----------------
-		// update the contents of the jlabel to be the image from the selected file
-		// ----------------
-
-	//	Image image = Toolkit.getDefaultToolkit().getImage(f.getPath());
-		try {
-			inkPanel.setImage(ImageIO.read(f));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
-
-	}
-
-	private void saveFile(File f) throws IOException {
-
-		// ----------------
-		// Take all the contents of the jpanel and save them to a png 
-		// 		destination is the file they selected via the filechooser
-		// ----------------
-		Container c = inkPanel;
-		BufferedImage im = new BufferedImage(c.getWidth(), c.getHeight(), BufferedImage.TYPE_INT_ARGB);
-		c.paint(im.getGraphics());
-		ImageIO.write(im, ".png", f );
 	}
 
 	@Override
@@ -510,6 +349,12 @@ class DrawFrame extends JFrame implements MouseMotionListener, MouseListener, Ac
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
--- src/ToolBar.java
+++ src/ToolBar.java
@@ -1,4 +1,164 @@
 
-public class ToolBar {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JToolBar;
+
+public class ToolBar implements ActionListener
+{
+	private JToolBar toolBar;
+	
+	private JButton select;
+	private JButton open;
+	private JButton pencil;
+	private JButton line;
+	private JButton rectangle;
+	private JButton circle;
+	private JButton oval;
+	private JButton rotateLeft;
+	private JButton rotateRight;
+	private JButton flipHorizontal;
+	private JButton flipVertical;
+	private JButton text;
+	private JButton erase;
+	private JButton fill;
+	private JButton undo;
+	private JButton redo;
+	private JButton clear;
+	private JComboBox comboBox;
+	
+	private DrawFrame frame;
+	private PaintPanel paint;
+	
+    public ToolBar(DrawFrame frame, PaintPanel paint) 
+	{
+    	this.frame = frame;
+    	this.paint = paint;
+    	
+    	this.initializeToolBar();
+		
+		clear.addActionListener(this);
+		rectangle.addActionListener(this);
+		line.addActionListener(this);
+		circle.addActionListener(this);
+		erase.addActionListener(this);
+		pencil.addActionListener(this);
+		comboBox.addActionListener(this);
+	}
+    
+	private void initializeToolBar() {
+		// ----------------
+		// create buttons for the tool bar
+		// ----------------
+		toolBar = new JToolBar(JToolBar.VERTICAL);
+		toolBar.setFloatable(false);		
+		
+		select = new JButton("Select");
+		//open = new JButton("Open");
+		pencil = new JButton("Pencil");
+		line = new JButton("Line",new ImageIcon(this.getClass().getResource("/icons/Line-24.png")));
+		rectangle = new JButton("Rectangle",new ImageIcon(this.getClass().getResource("/icons/Rectangle-24.png")));
+		circle = new JButton("Circle",new ImageIcon(this.getClass().getResource("/icons/Circled.png")));
+		//oval = new JButton("Oval");
+		rotateLeft = new JButton("Rotate left",new ImageIcon(this.getClass().getResource("/icons/Rotate Left-24.png")));
+		rotateRight  = new JButton("Rotate right",new ImageIcon(this.getClass().getResource("/icons/Rotate Right-24.png")));
+		flipHorizontal = new JButton("Flip horizontal",new ImageIcon(this.getClass().getResource("/icons/Flip Horizontal-24.png")));
+		flipVertical  = new JButton("Flip vertical",new ImageIcon(this.getClass().getResource("/icons/Flip Vertical-24.png")));
+		text = new JButton("Text");
+		erase = new JButton("Erase",new ImageIcon(this.getClass().getResource("/icons/Eraser-24.png")));
+		fill = new JButton("Fill",new ImageIcon(this.getClass().getResource("/icons/Fill Color-24.png")));
+		undo = new JButton("Undo",new ImageIcon(this.getClass().getResource("/icons/Undo-24.png")));
+		redo = new JButton("Redo",new ImageIcon(this.getClass().getResource("/icons/Redo-24.png")));
+		clear = new JButton("Clear");
+		
+		//select.setBackground(background);
+		//open.setBackground(background);
+		//pencil.setBackground(background);
+		
+		String[] items = {"2","3","4","5","6","7","8","9","10"};
+		comboBox = new JComboBox(items);
+		
+		// ----------------
+		// add buttons to the tool bar
+		// ----------------
+		
+		toolBar.add(select);
+		//toolBar.add(open);
+		toolBar.addSeparator();
+		toolBar.add(pencil);
+		toolBar.add(line);
+		toolBar.add(rectangle);
+		toolBar.add(circle);
+		//toolBar.add(oval);
+		toolBar.addSeparator();
+		toolBar.add(rotateLeft);
+		toolBar.add(rotateRight);
+		toolBar.add(flipHorizontal);
+		toolBar.add(flipVertical);
+		toolBar.addSeparator();
+		toolBar.add(text);
+		toolBar.add(erase);
+		toolBar.add(fill);
+		toolBar.addSeparator();
+		toolBar.add(undo);
+		toolBar.add(redo);
+		toolBar.addSeparator();
+		toolBar.add(clear);
+		toolBar.add(comboBox);
+	}
+    
+	public void actionPerformed(ActionEvent ae)
+	{
+		Object source = ae.getSource();
+
+		if (source == clear){
+			paint.clear();
+		}
+		else if (source == pencil){
+			paint.setTool(0);
+		}
+		else if (source == line) {
+			paint.setTool(1);
+		}
+		else if (source == rectangle) {
+			paint.setTool(2);
+		}
+		else if (source == circle) {
+			paint.setTool(3);
+		}
+		else if (source == select){
+			paint.setTool(4);
+		}
+		else if (source == text){
+			paint.setTool(5);
+		}
+		else if (source == erase){
+			paint.eraser();
+		}
+//		else if (source == thicker){
+//		
+//			inkPanel.thicker();
+//		}
+//		else if (source == thinner){
+//	
+//			inkPanel.thinner();
+//		}
+		else if (source == comboBox){
+			System.out.println(1);
+			JComboBox combo = (JComboBox)ae.getSource();
+			String current = (String) combo.getSelectedItem();
+			paint.setThickness(Float.valueOf(current) );
+		}
+		else{
+			JButton b = (JButton) source;
+			paint.setColor(b.getBackground());
+		}
+	}
+    public JToolBar getToolBar()
+    {
+    	return this.toolBar;
+    }
 
 }
