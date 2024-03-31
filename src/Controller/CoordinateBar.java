2effaa6b70578b9f9cfc1e4860c6c1f7924357ec
Adnan Zaki <zaki.adnan6@gmail.com>
2024-03-31 14:43:35 +0530

    New Icons for Coordinate Bar.

--- src/Controller/CoordinateBar.java
+++ src/Controller/CoordinateBar.java
@@ -1,6 +1,7 @@
 package Controller;
 
 import java.awt.Color;
+import java.awt.Image;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
@@ -13,7 +14,7 @@ public class CoordinateBar extends JToolBar {
 	private Separator separator;
 
 	public CoordinateBar() {
-		JLabel coordinatePic = new JLabel(new ImageIcon(getClass().getResource("/icons/coordinates.png")));
+		JLabel coordinatePic = new JLabel(resizeImageIcon("/icons/coordinates.png", 22, 22));
 		this.add(coordinatePic);
 		coordinates = new JLabel();
 		coordinates.setText("  0 x 0  ");
@@ -22,13 +23,19 @@ public class CoordinateBar extends JToolBar {
 		separator = new Separator();
 		this.add(separator);
 
-		JLabel sizePic = new JLabel(new ImageIcon(getClass().getResource("/icons/size.png")));
+		JLabel sizePic = new JLabel(resizeImageIcon("/icons/size.png", 24, 24));
 		this.add(sizePic);
 		frameSize = new JLabel();
 		frameSize.setText("  0 x 0  ");
 		this.add(frameSize);
 		this.setFloatable(false);
 		this.setBorder(BorderFactory.createMatteBorder(1, 0, 0, 0, Color.BLACK));
+	}
+
+	private ImageIcon resizeImageIcon(String imagePath, int width, int height) {
+		ImageIcon icon = new ImageIcon(this.getClass().getResource(imagePath));
+		Image image = icon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+		return new ImageIcon(image);
 	}
 
 	public JLabel getCoordinates() {
